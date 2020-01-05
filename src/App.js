38d666ddd2c5f13292b5d58905f08f5a78673bfc
mobcoder-pankaj <56378955+mobcoder-pankaj@users.noreply.github.com>
2020-01-05 11:01:54 +0530

    Update App.js
    test 22

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to pk reload.
+          Edit <code>src/App.js</code> and save to pk second time reload.
         </p>
         <a
           className="App-link"
