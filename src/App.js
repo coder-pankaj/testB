648412cdd43d4a950db49e403a86c768b5774776
mobcoder-pankaj <56378955+mobcoder-pankaj@users.noreply.github.com>
2020-01-05 18:26:06 +0530

    Update App.js
    tst

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> avi 22 todhkaya code.
+          Edit <code>src/App.js</code> test101.
         </p>
         <a
           className="App-link"
